645dbbc84aa1230f6234f72287654b17a17b887b
like-twelve-sixteenths-of-a-square <enzo.ressa@angrykitten.com>
2023-10-09 12:54:56 -0700

    Frotnite
    amonger

--- Assets/Course Library/Sips/PlayerControl.cs
+++ Assets/Course Library/Sips/PlayerControl.cs
@@ -10,21 +10,25 @@ public class PlayerControl : MonoBehaviour
     public bool isOnGround = true;
     public bool gameOver = false;
 
+    //This space exists only to prevent the massive line of variables from getting longer without a break
+    private Animator animator;
+
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
         Physics.gravity *= gravityModifier;
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && Input.GetKey(KeyCode.LeftShift)) { playerRb.AddForce(Vector3.up * jumpForce * 2, ForceMode.Impulse); isOnGround = false; }
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround) { playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse); isOnGround = false; }
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && Input.GetKey(KeyCode.LeftShift) && !gameOver) { playerRb.AddForce(Vector3.up * jumpForce * 2, ForceMode.Impulse); isOnGround = false; animator.SetTrigger("Jump_trig"); }
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver) { playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse); isOnGround = false; animator.SetTrigger("Jump_trig"); }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Ground")) { isOnGround = true; }
-        else if (collision.gameObject.CompareTag("Obstacle")) { gameOver = true; Debug.Log("R.I.P. Bozo"); }
+        else if (collision.gameObject.CompareTag("Obstacle")) { gameOver = true; Debug.Log("R.I.P. Bozo"); animator.SetBool("Death_b", true); animator.SetInteger("DeathType_int", 1); }
     }
 }
---
+++ Assets/Intro to 3D Animation/Animated Characters/Scripts/AnimatedPlayerController.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatedPlayerController : MonoBehaviour
+{
+    //Movement Variables
+    private float verticalInput;
+    public float moveSpeed;
+
+    private float horizontalInput;
+    public float turnSpeed;
+
+    //Jumping Variables
+    private Rigidbody rb;
+    public float jumpForce;
+    public bool isOnGround;
+
+    //Aimation Variadsmsfd
+    private Animator animator;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Get Components
+        rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Forward and Backward Movement
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed * verticalInput);
+
+        //Run/Idle babygirl oh yeah
+        animator.SetFloat( "verticalInput", Mathf.Abs (verticalInput) );
+
+
+        //Rotation
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime * horizontalInput);
+
+        //Jumping
+        if(Input.GetKeyDown(KeyCode.Space)  && isOnGround)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+            animator.SetBool("isOnGround", isOnGround);
+        }
+
+        //Gun
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            animator.SetTrigger("schuute");
+        }
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+            animator.SetBool("isOnGround", isOnGround);
+        }
+    }
+}
